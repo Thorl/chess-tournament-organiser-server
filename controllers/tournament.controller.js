c43b45c8ab9604ba62e989e4d2958ccb096d550c
omarali <o.ali94@gmail.com>
2023-03-23 13:18:14 +0000

    createTournament and getTournament (with populate)

--- controllers/tournament.controller.js
+++ controllers/tournament.controller.js
@@ -1,8 +1,36 @@
-const Tournament = require('../models/Tournament.model')
+const id = require("faker/lib/locales/id_ID");
+const Tournament = require("../models/Tournament.model");
 
-const createTournament = async (req,res,next) => {
-    
-}
+const createTournament = async (req, res, next) => {
+  const { name, _class, students, numberOfRounds } = req.body;
+  let participantsData = [];
 
+  for (x of students) {
+    participantsData.push({ participantID: x });
+  }
 
-module.exports = {}
+  const { _id: organiser } = req.payload;
+
+  await Tournament.create({
+    name,
+    _class,
+    participantsData,
+    numberOfRounds,
+    organiser,
+  });
+  res.send("tournament created");git 
+};
+
+const getTournament = async (req, res, next) => {
+  try {
+    const { tournamentId: id } = req.params;
+    const individualTournament = await Tournament.findById(id)
+      .populate("_class")
+      .populate("participantsData.participantID");
+    res.json({ individualTournament });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { createTournament, getTournament };
