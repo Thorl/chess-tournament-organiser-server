a8deb13fb3c24a7f7935ea25a98cf5d473a687ab
Thor Löfgren <lofgrenthor@gmail.com>
2023-03-23 20:39:01 +0100

    feat(getTournaments controller): Create controller
    - Add a "/" route in the tournaments.controller.js file and add a
    "getTournaments" controller to it.
    
    - Add a "tournaments" field to the Teacher model. The field should take
    an array of tournament IDs.
    
    - Update the “createTournament” controller with functionality to add the
    created tournament to the Teacher model.
    
    - Make the “school” field on the Class model required.
    
    - Improve the way data is fetched from the database by using projections.

--- controllers/tournaments.controller.js
+++ controllers/tournaments.controller.js
@@ -1,38 +1,67 @@
 // const id = require("faker/lib/locales/id_ID");
 const Tournament = require("../models/Tournament.model");
+const Teacher = require("../models/Teacher.model");
 
 const createTournament = async (req, res, next) => {
-  const { name, _class, students, numberOfRounds } = req.body;
+  try {
+    const { name, _class, students, numberOfRounds } = req.body;
 
-  const participantsData = [];
+    const participantsData = [];
 
-  for (const studentId of students) {
-    participantsData.push({ participantID: studentId });
+    for (const studentId of students) {
+      participantsData.push({ participantID: studentId });
+    }
+
+    const { _id: organiser } = req.payload;
+
+    const createdTournament = await Tournament.create({
+      name,
+      _class,
+      participantsData,
+      numberOfRounds,
+      organiser,
+    });
+
+    await Teacher.findByIdAndUpdate(organiser, {
+      $push: { tournaments: createdTournament },
+    });
+
+    res.sendStatus(201);
+  } catch (error) {
+    next(error);
   }
+};
 
-  const { _id: organiser } = req.payload;
+const getTournaments = async (req, res, next) => {
+  try {
+    const { _id: teacherId } = req.payload;
 
-  await Tournament.create({
-    name,
-    _class,
-    participantsData,
-    numberOfRounds,
-    organiser,
-  });
-  res.sendStatus(201);
+    const tournaments = await Teacher.findById(
+      teacherId,
+      "tournaments"
+    ).populate({
+      path: "tournaments",
+      populate: { path: "_class" },
+    });
+
+    res.json(tournaments);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getTournamentDetails = async (req, res, next) => {
   try {
     const { tournamentId } = req.params;
 
-    const individualTournament = await Tournament.findById(tournamentId)
+    const tournament = await Tournament.findById(tournamentId)
       .populate("_class")
       .populate("participantsData.participantID");
-    res.json({ individualTournament });
+
+    res.json(tournament);
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { createTournament, getTournamentDetails };
+module.exports = { createTournament, getTournamentDetails, getTournaments };
